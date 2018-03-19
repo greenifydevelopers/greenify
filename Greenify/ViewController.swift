aee1435ed8febca074c36bf85807932c0577c3ef
Ryan Schalk <schalkr1@mymail.nku.edu>
2018-03-19 16:45:21 -0400

    Created single-view Xcode project.

---
+++ Greenify/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Greenify
+//
+//  Created by Ryan Schalk on 3/19/18.
+//  Copyright © 2018 Greenify. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
