82f30fef913f6d88453729f82a896eb3d6ac61a9
Erik Seitz <erikseitz001@gmail.com>
2018-04-16 16:58:11 -0400

    Added file structure

---
+++ Greenify/ChooseHome/ChooseHomeTableViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ChooseHomeTableViewController.swift
+//  Greenify
+//
+//  Created by Erik Seitz on 4/16/18.
+//  Copyright © 2018 Greenify. All rights reserved.
+//
+
+import UIKit
+
+class ChooseHomeTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    
+    
+    
+    
+}
