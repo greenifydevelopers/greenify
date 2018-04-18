a696c7cae11161cbf900d666e8a7a32b81df5de4
Brendan Slack <slackb1@nku083348.hh.nku.edu>
2018-04-18 17:29:19 -0400

    view controller for information learn screen

---
+++ Greenify/Learn/ItemDetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ItemDetailViewController.swift
+//  Greenify
+//
+//  Created by Brendan Slack on 4/18/18.
+//  Copyright © 2018 Greenify. All rights reserved.
+//
+
+import UIKit
+
+class ItemDetailViewController: UIViewController {
+
+    @IBOutlet weak var detailText: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        var myText = "This furnace sucks"
+        
+        detailText.text = myText
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
